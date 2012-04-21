501551131b0794c82e5d24f43577646c20bc3556
Thomas Levine <thomas.levine@gmail.com>
2012-04-21 16:45:30 -0400

    oeu

--- foo.r
+++ foo.r
@@ -1,4 +1,5 @@
 library(sqldf)
+library(plyr)
 
 top_taxa <- function(){
   top_10_taxa.df <- sqldf('select taxon_family, count(taxon_family) as "count" from cites group by taxon_family order by count(taxon_family) desc limit 10', dbname = 'cites.db')
@@ -7,6 +8,20 @@ top_taxa <- function(){
   barplot(top_10_taxa)
 }
 
-by_country <- function(){
+by_taxon_and_country <- function(){
   top_10_taxa_by_country <- sqldf('select shipment_year, export_country_code, taxon_family, sum(quantity_1) as "export" from cites where taxon_family in (select taxon_family from cites group by taxon_family order by count(taxon_family) desc limit 10) group by shipment_year || export_country_code || taxon_family;', dbname = 'cites.db')
+  by_taxon <- ddply(top10, c('shipment_year', 'taxon_family'), function(df) { sum(df$export)})
+}
+
+
+# Sudden changes
+  # Down
+    # Varanidae
+
+  # Up
+    # Crocodylidae
+    # Ursidae
+
+find_huge_changes <- function(by_taxon_and_country.df){
+  by_taxon_and_country.df
 }
