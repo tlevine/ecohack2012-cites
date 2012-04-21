98db3c526f414ab57965fc6f0a0f6535010a831d
Thomas Levine <thomas.levine@gmail.com>
2012-04-21 16:23:54 -0400

    oeu

--- foo.r
+++ foo.r
@@ -6,3 +6,7 @@ top_taxa <- function(){
   names(top_10_taxa) <- top_10_taxa$taxon_family
   barplot(top_10_taxa)
 }
+
+by_country <- function(){
+  top_10_taxa_by_country <- sqldf('select shipment_year, export_country_code, taxon_family, sum(quantity_1) as "export" from cites where taxon_family in (select taxon_family from cites group by taxon_family order by count(taxon_family) desc limit 10) group by shipment_year || export_country_code || taxon_family;', dbname = 'cites.db')
+}
